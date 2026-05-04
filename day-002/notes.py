b5208bf15d3fe02b4f71ff770a360e6f4210443e
Constanza Riquelme <constanza.ri@gmail.com>
2026-05-03 20:08:40 -0400

    feat: add notes day 2

---
+++ day-002/notes.py
@@ -0,0 +1,36 @@
+# 2.14
+# Subscripting
+print("Hello"[0])   # Output: H
+print("Hello"[4])   # Output: o
+
+# String
+print("123" + "345")
+
+# Integer = whole number
+print(123 + 345)
+
+# Large Integers
+"""
+You can write large integers with _ between for better reading
+Python will ignore the _ and treat it as just a large integer
+"""
+print(123_456_789)  # Output: 123456789
+print(123456789)    # Output: 123456789 -- same as line 17
+
+# Float = Floating Point Number
+print(3.14159)      # ! It's a dot, not a comma
+
+# Boolean
+print(True)
+print(False)
+
+# 2.15
+print(type("abc"))  # Output: <class 'str'>
+print(type(123))    # Output: <class 'int'>
+print(type(3.14))   # Output: <class 'float'>
+print(type(True))   # Output: <class 'bool'>
+
+
+
+
+
