786ae7c62a92824360ea21acfed8f3e7e02f02b0
Constanza Riquelme <44560427+raiksha@users.noreply.github.com>
2026-05-04 21:01:34 -0400

    feat: add more notes day 2

--- day-002/notes.py
+++ day-002/notes.py
@@ -30,7 +30,42 @@ print(type(123))    # Output: <class 'int'>
 print(type(3.14))   # Output: <class 'float'>
 print(type(True))   # Output: <class 'bool'>
 
+# This line gives TypeError
+# print("Number of letters in your name: " + len(input("Enter your name: ")))
 
+# Correct way:
+length_of_name = len(input("Enter your name: "))
+print("Number of letters in your name: " + str(length_of_name))
 
+# 2.16
+print("My age: " + str(28))
+print(123+ 456)
+print(7 - 2)
+print(3 * 2)
+print(5 / 3)    # Returns a float
+print(5 // 3)   # Returns a int (truncated)
+print(2 ** 3)   # Power of/Exponents
+
+# Priority PEMDAS/LR -> (), **, * or /, + or -. In same level, from left to right
+print(3 * 3 + 3 / 3 - 3)    # Output: 7
+print(3 * (3 + 3) / 3 - 3)    # Output: 3
+
+# 2.17
+bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi))
+print(round(bmi, 2))    # Number to round, number of decimals. Ex: 30.85
+
+# Number manipulation
+score = 1
+
+score += 1  # Same as score = score + 1
+score -= 1  # Same as score = score - 1
+score *= 1  # Same as score = score * 1
+score /= 1  # Same as score = score / 1
+
+# f-strings
+print(f"Your score is {score}")
 
 
